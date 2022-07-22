9a7f447371b6e2b1491860df7d05f7d7fb62c87e
Claire <97535035+CLg003@users.noreply.github.com>
2022-07-22 10:29:02 +0100

    Profile class constructor.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace DatingProfile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Profile sam = new Profile("Sam Drakkila", 30, "New York", "USA", "he/him");
+        }
+    }
+}
